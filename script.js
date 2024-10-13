0ec4f996bb904a9b728458d210803a2ace8cbd76
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-13 15:37:28 -0500

    Recordar arreglos y objetos

--- script.js
+++ script.js
@@ -4,6 +4,17 @@
 // console.log(ready)
 // console.log(typeof(ready)
 
-const res = prompt('¿Cuál es tu lenguaje de programación favorito?', 'JavaScript')
-console.log(res)
-console.log(typeof(res))
+// const res = prompt('¿Cuál es tu lenguaje de programación favorito?', 'JavaScript')
+// console.log(res)
+// console.log(typeof(res))
+
+const array = ['HTML', 'CSS', 'JavaScript', 'ReactJS']
+
+const object = { nombre: 'Hugo Andres', ocupacion: 'Desarrollador', materia: 'JavaScript para DOM' }
+
+const objectArray = [
+    {materia: 'HTML', dificultad: 6, necesario: true},
+    {materia: 'CSS', dificultad: 8, necesario: true},
+    {materia: 'JavaScript', dificultad: 9.5, necesario: false},
+    {materia: 'ReactJS', dificultad: 8, necesario: false}
+]
