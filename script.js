165e9599f5da383521e8d8b8369bd542559b3907
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-13 14:33:08 -0500

    Alert, prompt y confirm

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-window.alert("Bienvenido al curso de JavaScript para DOM");
+// alert("Bienvenido al curso de JavaScript para DOM");
+
+// const ready = confirm("¿Hiciste los cursos de Maquetación Web y Programación imperativa?")
+// console.log(ready)
+// console.log(typeof(ready)
+
+const res = prompt('¿Cuál es tu lenguaje de programación favorito?', 'JavaScript')
+console.log(res)
+console.log(typeof(res))
