8434eb006f5933d7c93eb54c5bcd2295ee5d323c
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-13 16:34:07 -0500

    DOM DOcument Object Model

--- script.js
+++ script.js
@@ -8,13 +8,20 @@
 // console.log(res)
 // console.log(typeof(res))
 
-const array = ['HTML', 'CSS', 'JavaScript', 'ReactJS']
+// const array = ['HTML', 'CSS', 'JavaScript', 'ReactJS']
 
-const object = { nombre: 'Hugo Andres', ocupacion: 'Desarrollador', materia: 'JavaScript para DOM' }
+// const object = { nombre: 'Hugo Andres', ocupacion: 'Desarrollador', materia: 'JavaScript para DOM' }
 
-const objectArray = [
-    {materia: 'HTML', dificultad: 6, necesario: true},
-    {materia: 'CSS', dificultad: 8, necesario: true},
-    {materia: 'JavaScript', dificultad: 9.5, necesario: false},
-    {materia: 'ReactJS', dificultad: 8, necesario: false}
-]
+// const objectArray = [
+//     {materia: 'HTML', dificultad: 6, necesario: true},
+//     {materia: 'CSS', dificultad: 8, necesario: true},
+//     {materia: 'JavaScript', dificultad: 9.5, necesario: false},
+//     {materia: 'ReactJS', dificultad: 8, necesario: false}
+// ]
+
+//document.getElementById('test').innerHTML = 'Hola Mundo desde JavaScript'
+//document.getElementById('test').style.color = 'red'
+document.getElementById('test').setAttribute('class', 'colorado')
+// console.log(document.getElementById('test'))
+// console.log(document.getElementsByClassName('test'))
+// console.log(document.getElementsByTagName('p'))
