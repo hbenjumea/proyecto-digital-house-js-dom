7b60612000d0dd6cf2b85bd311d2a8af3116e4b5
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-13 21:22:38 -0500

    DOM selectors query and form

--- script.js
+++ script.js
@@ -30,10 +30,42 @@
 
 // document.getElementById('test').innerHTML = 'En este párrafo vamos a estar escribiendo el primero de los saludos, el cual es: ' + salutations[0].innerHTML
 
-const form = document.forms['form1'];
-let text = '';
-for (let i = 0; i < form.length; i++) {
-    text += form.elements[i].value + '<br>';    
-}
+// const form = document.forms['form1'];
+// let text = '';
+// for (let i = 0; i < form.length; i++) {
+//     text += form.elements[i].value + '<br>';    
+// }
 
-document.getElementById('test').innerHTML = text;
+// document.getElementById('test').innerHTML = text;
+
+// DECLARATIVE FUNCTIONS
+// function changeThisElement(element) {
+//     element.innerHTML = 'Hola mundo desde función declarativa'
+// }
+
+// ARROW FUNCTIONS
+// const changeThisElement = element => element.innerHTML = 'Hola mundo desde función flecha'
+
+// const displayDate = () => document.getElementById('test').innerHTML = Date()
+
+// document.getElementById('btn').onclick = displayDate // No abro y cierro parentesis porque quiero que se ejecute cuando se de la acción
+
+// const checkCookies = () => {
+//     let text = navigator.cookieEnabled ? 'Cookies habilitadas' : 'Cookies no habilitadas';
+//     document.getElementById('test').innerHTML = text;
+// }
+
+// const upperCase = () => {
+//     const input = document.getElementById('name');
+//     input.value = input.value.toUpperCase();
+// }
+
+// const mouseOver = element => {
+//     element.innerHTML = 'Estás sobre el div'
+// }
+// const mouseOut = element => {
+//     element.innerHTML = 'Estás fuera del div'
+// }
+const changeStyle = element => {
+    element.style.background = 'aqua'
+}
