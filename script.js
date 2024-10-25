5497bff796a4633c924f716c435dd1adb8b83eec
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-24 20:00:35 -0500

    Manipular nodos en el dom

--- script.js
+++ script.js
@@ -70,15 +70,35 @@
 //     element.style.background = 'aqua'
 // }
 
-let key
+//let key
 
 // const keydown = ($event) => {
 //     key = $event.altKey
 //     console.log(key)
 //     document.getElementById('alt').innerHTML = key ? 'Tocando ALT' : 'No tocando ALT'
 // }
-const keydown = ($event) => {
-    key = $event.getModifierState('CapsLock')
-    console.log(key)
+// const keydown = ($event) => {
+//     key = $event.getModifierState('CapsLock')
+//     console.log(key)
     
-}
+// }
+
+// Creando un nodo párrafo
+const paragraph = document.createElement('p')
+// Acá creamos un elemento de texto
+const node = document.createTextNode('Este es contenido nuevo')
+// Incorporamos el elemento de texto al nodo de párrafo
+paragraph.appendChild(node)
+
+// En la variable container tenemos el DIV
+const container = document.getElementById('div1')
+// const p1 = document.getElementById('p1')
+// //container.appendChild(paragraph)
+// container.insertBefore(paragraph, p1)
+
+// const p2 = document.getElementById('p2')
+// p2.remove()
+// container.removeChild(p1)
+
+const p3 = document.getElementById('p3')
+container.replaceChild(paragraph, p3)
