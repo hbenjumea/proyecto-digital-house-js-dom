3ce18175eea2ca709182b3d7dbfc996162ded169
Hugo Andres <hugo_andresb@hotmail.com>
2024-10-24 20:21:29 -0500

    Agregar y quitar  clases

--- script.js
+++ script.js
@@ -84,21 +84,33 @@
 // }
 
 // Creando un nodo párrafo
-const paragraph = document.createElement('p')
-// Acá creamos un elemento de texto
-const node = document.createTextNode('Este es contenido nuevo')
-// Incorporamos el elemento de texto al nodo de párrafo
-paragraph.appendChild(node)
+// const paragraph = document.createElement('p')
+// // Acá creamos un elemento de texto
+// const node = document.createTextNode('Este es contenido nuevo')
+// // Incorporamos el elemento de texto al nodo de párrafo
+// paragraph.appendChild(node)
 
-// En la variable container tenemos el DIV
-const container = document.getElementById('div1')
-// const p1 = document.getElementById('p1')
-// //container.appendChild(paragraph)
-// container.insertBefore(paragraph, p1)
+// // En la variable container tenemos el DIV
+// const container = document.getElementById('div1')
+// // const p1 = document.getElementById('p1')
+// // //container.appendChild(paragraph)
+// // container.insertBefore(paragraph, p1)
 
-// const p2 = document.getElementById('p2')
-// p2.remove()
-// container.removeChild(p1)
+// // const p2 = document.getElementById('p2')
+// // p2.remove()
+// // container.removeChild(p1)
 
-const p3 = document.getElementById('p3')
-container.replaceChild(paragraph, p3)
+// const p3 = document.getElementById('p3')
+// container.replaceChild(paragraph, p3)
+
+// const addClass = () => {
+//     const divClassList = document.getElementById('div1').classList
+//     divClassList.add('red-background')
+// }
+// const removeClass = () => {
+//     document.getElementById('div1').classList.remove('red-background')    
+// }
+ const toggleClass = () => {
+     document.getElementById('div1').classList.toggle('red-background')    
+ }
+
